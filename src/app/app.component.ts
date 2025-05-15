90b7934317c28a7f16286e9296c01a3fff9f560f
Santosh Yadav <santosh.yadav198613@gmail.com>
2025-05-15 22:07:30 +0200

    add resource api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component, computed, effect, OnInit } from '@angular/core';
+import {
+  Component,
+  computed,
+  effect,
+  linkedSignal,
+  OnInit,
+  resource,
+  untracked,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { httpResource } from '@angular/common/http';
 import { JsonPipe } from '@angular/common';
 import { TodoTableComponent } from './todo-table/todo-table.component';
 import { Todo } from './todo.model';
+import { User } from './user.model';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { signal } from '@angular/core';
+import { fromFetch } from 'rxjs/fetch';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TodoTableComponent],
+  imports: [RouterOutlet, TodoTableComponent, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -18,6 +29,11 @@ export class AppComponent implements OnInit {
 
   firstName = signal('Morgan');
   tempFirstName = signal('Morgan');
+  counter = signal(0);
+
+  // New signals for dropdown
+  dropdownValues = signal(['Option 1', 'Option 2', 'Option 3']);
+  selectedValue = linkedSignal(() => this.dropdownValues()[0]);
 
   todoList = httpResource<Todo[]>(
     () => `https://jsonplaceholder.typicode.com/todos`
@@ -25,8 +41,35 @@ export class AppComponent implements OnInit {
 
   firstNameCapitalized = computed(() => this.firstName().toUpperCase());
 
-  effectFn = effect(() => {
-    console.log(`The name is: ${this.firstName()}`);
+  effectFn = effect((oncleanup) => {
+    console.log(
+      `The name is: ${this.firstName()} and count ${untracked(this.counter)}`
+    );
+  });
+
+  userId = signal(1);
+
+  userRxResource = rxResource({
+    request: () => ({ id: this.userId() }),
+    loader: ({ request }): Observable<User> =>
+      fromFetch(`https://jsonplaceholder.typicode.com/users/${request.id}`, {
+        selector: (response) => response.json(),
+      }),
+  });
+
+  userResource = resource({
+    request: () => ({ id: this.userId() }),
+    loader: ({ request }): Promise<User> => {
+      // You need to parse the JSON response from fetch
+      return fetch(
+        `https://jsonplaceholder.typicode.com/users/${request.id}`
+      ).then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      });
+    },
   });
 
   ngOnInit(): void {}
@@ -34,6 +77,7 @@ export class AppComponent implements OnInit {
   onInputChange(event: Event): void {
     const input = event.target as HTMLInputElement;
     this.tempFirstName.set(input.value);
+    this.counter.update((c) => c + 1);
   }
 
   updateFirstName(): void {
@@ -42,5 +86,22 @@ export class AppComponent implements OnInit {
 
   stopLogging() {
     this.effectFn.destroy();
+  }
+
+  onDropdownChange(event: Event): void {
+    const select = event.target as HTMLSelectElement;
+    this.selectedValue.set(select.value);
+  }
+
+  updateOptions() {
+    this.dropdownValues.set(['Ground', 'Air', 'Sea']);
+  }
+
+  nextUser() {
+    this.userId.update((userId) => userId + 1);
+  }
+
+  reloadUser() {
+    this.userResource.reload();
   }
 }
